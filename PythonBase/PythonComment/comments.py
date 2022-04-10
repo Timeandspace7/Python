d19e9fa168d2c148629de49ba4e3d7b8a85613a1
Timeandspace7 <48798867+Timeandspace7@users.noreply.github.com>
2022-04-10 12:02:07 +0800

    Create comments.py

---
+++ PythonBase/PythonComment/comments.py
@@ -0,0 +1,18 @@
+#this is a comment
+print('Hello World!')
+
+print('Hello World!') #this is a comment
+
+#print("Hello World")
+print("Welcome to pythonexamples.org")
+
+#this is a comment
+#this is another comment
+#one more comment
+print('Hello World!')
+
+'''
+this is 
+multi-line string and servers as a comment
+'''
+print('Hello World!')
